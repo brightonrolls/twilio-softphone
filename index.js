42c1123e4f8ab8a3ea12cd9185710992e902f57b
brightonrolls <125768324+brightonrolls@users.noreply.github.com>
2023-02-18 21:45:47 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+{
+  "name": "becks softphone",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/brightonrolls/twilio-softphone"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/brightonrolls/twilio-softphone"
+  },
+  "homepage": "https://github.com/brightonrolls/twilio-softphone#readme",
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
